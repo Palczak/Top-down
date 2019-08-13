a1b5f0684423da8174816e4160b58ab08bd38dca
Jakub Palczewski <jakubpala@poczta.onet.pl>
2019-08-13 22:25:24 +0200

    Enemy won't shoot through walls.

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -3,24 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemyControl : MonoBehaviour
 {
-    public GameObject Target;
+    private GameObject _target;
     private Movement _movement;
     private Combat _combat;
+    private int _layerMask;
 
     private void Start()
     {
+        _layerMask = ~LayerMask.GetMask("Enemy");
+        _target = GameObject.Find("Player");
         _movement = GetComponent<Movement>();
         _combat = GetComponent<Combat>();
     }
 
     private void Update()
     {
-        if(Target != null)
+        if (_target != null)
         {
-            _movement.LookAt(Target.transform.position);
-            _combat.Shoot(gameObject);
+            _movement.LookAt(_target.transform.position);
+            var hit = Physics2D.Raycast(transform.position, _target.transform.position - transform.position, Mathf.Infinity, _layerMask);
+
+            if (hit.rigidbody == _target.GetComponent<Rigidbody2D>())
+            {
+                _combat.Shoot(gameObject);
+            }
         }
     }
 
