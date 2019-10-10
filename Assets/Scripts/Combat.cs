d9cde51ca8dcd2936476b5adedcdb50f81c10fee
Jakub Palczewski <jakubpala@poczta.onet.pl>
2019-10-10 19:14:27 +0200

    Fixed walls and combat

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -10,27 +10,23 @@ public class Combat : MonoBehaviour
     [SerializeField]
     private int _currentHitPoints;
     [SerializeField]
-    private uint _pauseBetweenShots;
+    private float _pauseBetweenShots;
     [SerializeField]
-    private uint _shotTimer;
+    private float _shotTimer;
 
-    // Start is called before the first frame update
     private void Start()
     {
-        //fixed update 1 tick every 0.02 sec
-        //1 second have 50 ticks
-        //1 minute have 3000 ticks
-        _pauseBetweenShots = 3000 / FireRateRPM;
+        _pauseBetweenShots = 60f / FireRateRPM;
         _shotTimer = 0;
 
         _currentHitPoints = MaxHitPoints;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        if(_shotTimer <= _pauseBetweenShots)
+        if (_shotTimer <= _pauseBetweenShots)
         {
-            _shotTimer++;
+            _shotTimer += Time.deltaTime;
         }
     }
 
