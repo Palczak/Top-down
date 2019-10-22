4cf83f2bd44ce0a0b6188f7db119d97543f0829d
Jakub Palczewski <jakubpala@poczta.onet.pl>
2019-10-22 22:39:09 +0200

    Added logic nodes.

--- Assets/Scripts/GridNodes.cs
+++ Assets/Scripts/GridNodes.cs
@@ -1,23 +1,33 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts;
+using UnityEngine;
 
 public class GridNodes : MonoBehaviour
 {
     public GameObject Node;
-    private GameObject[,] _nodes { get; set; }
+    private Node[,] _nodes { get; set; }
 
     void Start()
     {
         Grid grid = gameObject.GetComponent<Grid>();
         var gridSize = GetComponent<RectTransform>().sizeDelta;
-        _nodes = new GameObject[(int)gridSize.x + 1, (int)gridSize.y + 1];
+        _nodes = new Node[(int)gridSize.x + 1, (int)gridSize.y + 1];
 
         for (int i = 0; i <= gridSize.x; i++)
         {
             for (int j = 0; j <= gridSize.y; j++)
             {
-                _nodes[i, j] = Instantiate(Node);
-                _nodes[i, j].transform.position = new Vector3((i - 1 + grid.cellSize.x) - gridSize.x / 2, (j - 1 + grid.cellSize.y) - gridSize.y / 2, 0);
+                int x = (int)((i - 1 + grid.cellSize.x) - gridSize.x / 2);
+                int y = (int)((j - 1 + grid.cellSize.y) - gridSize.y / 2);
+                _nodes[i, j] = new Node(x, y);
+                //Lines below are for debug only
+                GameObject node = Instantiate(Node);
+                node.transform.position = new Vector3(x, y, 0);
             }
+        }
+
+        foreach (var node in _nodes)
+        {
+            //raycast to chec connection betwen nodes
         }
     }
 }
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts
+{
+    public class Node
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        private List<Node> _neighbors { get; }
+
+        public Node (int x, int y)
+        {
+            X = x;
+            Y = Y;
+            _neighbors = new List<Node>();
+        }
+
+        public Node()
+        {
+            X = 0;
+            Y = 0;
+            _neighbors = new List<Node>();
+        }
+    }
+}
