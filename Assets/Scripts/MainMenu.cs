e43615f0cf083cb722f6f614eec6d4a5fe9f9c35
Jakub Palczewski <jakubpala@poczta.onet.pl>
2020-01-19 22:45:00 +0100

    Added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    private Toggle _hardMode;
+    void Awake()
+    {
+        _hardMode = FindObjectOfType<Toggle>();
+    }
+
+    public void StartGame()
+    {
+        Config.HardMode = _hardMode.isOn;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
