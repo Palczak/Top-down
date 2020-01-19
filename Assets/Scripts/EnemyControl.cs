b3dcb1dd4c2fe0156f8cd4b86c862df0757b1623
Jakub Palczewski <jakubpala@poczta.onet.pl>
2020-01-19 15:35:31 +0100

    Finished motion prediciton

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -1,9 +1,6 @@
 ﻿using Assets.Scripts;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System.Linq;
+using UnityEngine;
 
 public class EnemyControl : MonoBehaviour
 {
@@ -27,45 +24,79 @@ public class EnemyControl : MonoBehaviour
     {
         if (_target != null)
         {
-            var path = _pathFinding.FindPath(transform.position, _target.transform.position);
-            if (path.Count() != 0)
-            {
-                var moveTo = new Vector2(path.Last().X, path.Last().Y);
-                moveTo -= (Vector2)transform.position;
-                _movement.Move(moveTo);
-            }
+            MoveToTarget();
+            var rayHit = Physics2D.Raycast(transform.position, _target.transform.position - transform.position, Mathf.Infinity, _layerMask);
+            bool isTargetObstructed = rayHit.rigidbody == _target.GetComponent<Rigidbody2D>();
 
             if (Config.HardMode)
             {
-                Vector2 targetVelocity = _target.GetComponent<Rigidbody2D>().velocity;
-                float missileSpeed = _combat.Projectile.GetComponent<Movement>().Speed * 0.75f;
+                var predictedPosition = PredictTargetMovement();
 
-                float a = (targetVelocity.x * targetVelocity.x) + (targetVelocity.y * targetVelocity.y) - (missileSpeed * missileSpeed);
-                float b = 2 * (targetVelocity.x * (_target.transform.position.x - gameObject.transform.position.x)
-                    + targetVelocity.y * (_target.transform.position.y - gameObject.transform.position.y));
-                float c = ((_target.transform.position.x - gameObject.transform.position.x) * (_target.transform.position.x - gameObject.transform.position.x)) +
-                    ((_target.transform.position.y - gameObject.transform.position.y) * (_target.transform.position.y - gameObject.transform.position.y));
-  
-                float disc = b * b - (4 * a * c);
-
-                float t1 = (-1 * b + Mathf.Sqrt(disc)) / (2 * a);
-                float t2 = (-1 * b - Mathf.Sqrt(disc)) / (2 * a);
-                float t = Mathf.Max(t1, t2);// let us take the larger time value 
-                float aimX = (targetVelocity.x * t) + _target.transform.position.x;
-                float aimY = _target.transform.position.y + (targetVelocity.y * t);
-                _movement.LookAt(new Vector2(aimX, aimY));
-                _combat.Shoot(gameObject);//now position the gameObject
-
+                rayHit = Physics2D.Raycast(transform.position, predictedPosition - transform.position, Vector3.Distance(transform.position, predictedPosition), _layerMask);
+                if (rayHit.collider == null)
+                {
+                    rayHit = Physics2D.Raycast(predictedPosition, _target.transform.position - predictedPosition, float.PositiveInfinity, _layerMask);
+                    if (rayHit.rigidbody == _target.GetComponent<Rigidbody2D>())
+                    {
+                        //targetedPosition = predictedPosition;
+                        if(Random.value > 0.5f )
+                            LockOnPositionAndShoot(predictedPosition);   
+                        else   
+                            LockOnPositionAndShoot(_target.transform.position, isTargetObstructed);   
+                    }
+                    else
+                        LockOnPositionAndShoot(_target.transform.position, isTargetObstructed);
+                }
+                else
+                    LockOnPositionAndShoot(_target.transform.position, isTargetObstructed);
             }
             else
             {
-                _movement.LookAt(_target.transform.position);
-                var rayHit = Physics2D.Raycast(transform.position, _target.transform.position - transform.position, Mathf.Infinity, _layerMask);
-                if (rayHit.rigidbody == _target.GetComponent<Rigidbody2D>())
-                {
-                    _combat.Shoot(gameObject);
-                }
+                LockOnPositionAndShoot(_target.transform.position, isTargetObstructed);
             }
+            //_movement.LookAt(_target.transform.position);
+            //var rayHit = Physics2D.Raycast(transform.position, _target.transform.position - transform.position, Mathf.Infinity, _layerMask);
+            //rayHit.rigidbody == _target.GetComponent<Rigidbody2D>()
         }
+    }
+
+    private void LockOnPositionAndShoot(Vector3 target, bool shoot = true)
+    {
+        _movement.LookAt(target);
+        if (shoot)
+        {
+            _combat.Shoot(gameObject);
+        }
+    }
+
+    private void MoveToTarget()
+    {
+        var path = _pathFinding.FindPath(transform.position, _target.transform.position);
+        if (path.Count() != 0)
+        {
+            var moveTo = new Vector2(path.Last().X, path.Last().Y);
+            moveTo -= (Vector2)transform.position;
+            _movement.Move(moveTo);
+        }
+    }
+
+    private Vector3 PredictTargetMovement()
+    {
+        Vector2 targetVelocity = _target.GetComponent<Rigidbody2D>().velocity;
+        float missileSpeed = _combat.Projectile.GetComponent<Movement>().Speed * 0.75f;
+        float a = (targetVelocity.x * targetVelocity.x) + (targetVelocity.y * targetVelocity.y) - (missileSpeed * missileSpeed);
+        float b = 2 * (targetVelocity.x * (_target.transform.position.x - gameObject.transform.position.x)
+            + targetVelocity.y * (_target.transform.position.y - gameObject.transform.position.y));
+        float c = ((_target.transform.position.x - gameObject.transform.position.x) * (_target.transform.position.x - gameObject.transform.position.x)) +
+            ((_target.transform.position.y - gameObject.transform.position.y) * (_target.transform.position.y - gameObject.transform.position.y));
+        float disc = b * b - (4 * a * c);
+        float t1 = (-1 * b + Mathf.Sqrt(disc)) / (2 * a);
+        float t2 = (-1 * b - Mathf.Sqrt(disc)) / (2 * a);
+        float t = Mathf.Max(t1, t2);// let us take the larger time value 
+        float aimX = (targetVelocity.x * t) + _target.transform.position.x;
+        float aimY = _target.transform.position.y + (targetVelocity.y * t);
+
+        var predictedPosition = new Vector3(aimX, aimY);
+        return predictedPosition;
     }
 }
